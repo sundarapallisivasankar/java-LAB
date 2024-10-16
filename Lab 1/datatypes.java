222fc7328a01099998df0614cf21e72d6f32371e
sundarapallisivasankar <166827964+sundarapallisivasankar@users.noreply.github.com>
2024-10-16 12:06:46 +0530

    Create datatypes.java

---
+++ Lab 1/datatypes.java
@@ -0,0 +1,21 @@
+public class datatypes{
+	static int defaultInt;
+	static char defaultChar;
+	static boolean defaultBoolean;
+	static long defaultLong;
+	static short defaultShort;
+	static float defaultFloat;
+	static byte defaultByte;
+	static double defaultDouble;
+	public static void main(String[]args){
+		System.out.println("default value of Int:"+defaultInt );
+		System.out.println("default value of char:"+defaultChar);
+		System.out.println("default value of boolean:"+defaultBoolean);
+		System.out.println("default value of long:"+defaultLong);
+		System.out.println("default value of short:"+defaultShort);
+		System.out.println("default value of float:"+defaultFloat);
+		System.out.println("default value of byte:"+defaultByte);
+		System.out.println("default value of double:"+defaultDouble);
+	}
+	
+}
